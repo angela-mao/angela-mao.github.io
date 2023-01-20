4497c64a53547451eca98ebb0f67022c9eb3a9ff
Angela Mao <angela.mao.am@gmail.com>
2023-01-20 15:19:45 -0500

    Updated resume

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,12 +17,12 @@ const Header = (props) => {
                 <a href="/#/home" id="home">Home</a>
                 <a href="/#/about" id="about">About</a>
                 <a href="/#/experience" id="experience">Experience</a>
-                <a href="https://drive.google.com/file/d/1RVeWFV9I3dndYhxdF76Omk1bsNyW5xb5/view?usp=sharing" id="resume">Resume</a>
+                <a href="https://drive.google.com/file/d/1vdUxsVGvdkPawVqUamjhV3lu91Iy5XTx/view?usp=sharing" id="resume">Resume</a>
                 <DropdownButton id="icon" title={<img className="menu" src={menu} alt="Menu"/>}>
                     <Dropdown.Item href="/#/home" id="item1">Home</Dropdown.Item>
                     <Dropdown.Item href="/#/about" id="item2">About</Dropdown.Item>
                     <Dropdown.Item href="/#/experience" id="item3">Experience</Dropdown.Item>
-                    <Dropdown.Item href="https://drive.google.com/file/d/1RVeWFV9I3dndYhxdF76Omk1bsNyW5xb5/view?usp=sharing" id="item4">Resume</Dropdown.Item>
+                    <Dropdown.Item href="https://drive.google.com/file/d/1vdUxsVGvdkPawVqUamjhV3lu91Iy5XTx/view?usp=sharing" id="item4">Resume</Dropdown.Item>
                 </DropdownButton>
             </nav>
         </header>
